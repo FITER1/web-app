a006198243b74e27455a1c1280fd1ce262f5a0af
Deepika <deepika@fiter.io>
2021-02-09 17:55:22 +0530

    Add screen for view on hold transactions. https://fiterio.atlassian.net/browse/CN-32

---
+++ src/app/savings/common-resolvers/savings-account-onholdtransactions.resolver.ts
@@ -0,0 +1,32 @@
+/** Angular Imports */
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+
+/** rxjs Imports */
+import { Observable } from 'rxjs';
+
+/** Custom Services */
+import { SavingsService } from '../savings.service';
+
+/**
+ * Savings Account Transaction data resolver.
+ */
+@Injectable()
+export class SavingsAccountOnHoldTransactionsResolver implements Resolve<Object> {
+
+  /**
+   * @param {SavingsService} SavingsService Savings service.
+   */
+  constructor(private savingsService: SavingsService) { }
+
+  /**
+   * Returns the Savings Account guaranteed loans data.
+   * @param {ActivatedRouteSnapshot} route Route Snapshot
+   * @returns {Observable<any>}
+   */
+  resolve(route: ActivatedRouteSnapshot): Observable<any> {
+    const savingAccountId = route.parent.paramMap.get('savingAccountId');
+    return this.savingsService.getOnHoldTransactionsForSavingsAccount(savingAccountId);
+  }
+
+}
---
+++ src/app/savings/savings-account-view/view-onhold/view-onhold.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'mifosx-view-onhold',
+  templateUrl: './view-onhold.component.html',
+  styleUrls: ['./view-onhold.component.scss']
+})
+export class ViewOnholdComponent implements OnInit {
+
+  /** Client Guarantors Accounts */
+  guaranteesAccounts : any = [];
+  savingsAccountData: any;
+
+  openLoansColumns: string[] = ['Loan Product', 'Loan Account No', 'Amount', 'Remaining Amount', 'Status', 'View Transactions'];
+
+ /**
+  * 
+  * @param route 
+  */
+  constructor(private route : ActivatedRoute,
+              private router: Router) {
+    this.route.data.subscribe((data : {savingsOnHold : any, savingsAccount: any}) => {
+      this.guaranteesAccounts = data.savingsOnHold.guarantorAccounts;
+      this.savingsAccountData = data.savingsAccount;
+    });
+   }
+
+  ngOnInit(): void {
+  }
+}
