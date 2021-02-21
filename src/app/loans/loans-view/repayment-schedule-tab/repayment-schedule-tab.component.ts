150f23de456e3b6ae7b39d9a8c3d5969ecca84ad
venkata ganesh <vganesh.pokuru@gmail.com>
2021-02-21 22:42:51 +0530

    CN-74: adding print option for repayment schedule

--- src/app/loans/loans-view/repayment-schedule-tab/repayment-schedule-tab.component.ts
+++ src/app/loans/loans-view/repayment-schedule-tab/repayment-schedule-tab.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
+import { ReportsService } from 'app/reports/reports.service';
 
 @Component({
   selector: 'mifosx-repayment-schedule-tab',
@@ -14,19 +16,43 @@ export class RepaymentScheduleTabComponent implements OnInit {
   isWaived: boolean;
   /** Columns to be displayed in original schedule table. */
   displayedColumns: string[] = ['number', 'days', 'date', 'paiddate', 'check', 'principalDue', 'balanceOfLoan', 'interest', 'fees', 'penalties', 'due', 'paid', 'inadvance', 'late', 'waived', 'outstanding'];
+  loanId: string;
+  pentahoUrl: any;
+  showReport: boolean  = false;
 
   /**
    * Retrieves the loans with associations data from `resolve`.
    * @param {ActivatedRoute} route Activated Route.
    */
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private sanitizer: DomSanitizer,
+              private reportsService: ReportsService) {
     this.route.parent.data.subscribe((data: { loanDetailsData: any }) => {
       this.repaymentScheduleDetails = data.loanDetailsData.repaymentSchedule;
     });
+    this.loanId = this.route.parent.snapshot.paramMap.get('loanId');
   }
 
   ngOnInit() {
     this.isWaived = this.repaymentScheduleDetails.totalWaived > 0;
   }
 
+  generate() {
+    const data = {
+      'output-type':	'PDF',
+      R_selectLoan:	this.loanId
+    };
+    this.reportsService.getPentahoRunReportData('Loan Repayment schedule', data, 'default', 'en', 'dd MMMM yyyy')
+      .subscribe( (res: any) => {
+        const contentType = res.headers.get('Content-Type');
+        const file = new Blob([res.body], {type: contentType});
+        const filecontent = URL.createObjectURL(file);
+        this.pentahoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(filecontent);
+        this.showReport = true;
+      });
+  }
+
+  closeReport(){
+   this.showReport = false;
+  }
 }
