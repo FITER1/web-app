734d29ffaa217626169b0634905c276b1207da93
venkata ganesh <vganesh.pokuru@gmail.com>
2021-02-23 20:50:01 +0530

    CN-74: adding print original schedule

--- src/app/loans/loans-view/original-schedule-tab/original-schedule-tab.component.ts
+++ src/app/loans/loans-view/original-schedule-tab/original-schedule-tab.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ReportsService } from 'app/reports/reports.service';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'mifosx-original-schedule-tab',
@@ -12,18 +14,43 @@ export class OriginalScheduleTabComponent implements OnInit {
   originalScheduleDetails: any;
   /** Columns to be displayed in original schedule table. */
   displayedColumns: string[] = ['date', 'principalDue', 'balanceOfLoan', 'interest', 'fees', 'penalties', 'outstanding'];
+  loanId: string;
+  pentahoUrl: any;
+  showReport: boolean  = false;
 
   /**
    * Retrieves the loans with associations data from `resolve`.
    * @param {ActivatedRoute} route Activated Route.
    */
-  constructor(private route: ActivatedRoute) {
+  constructor(private route: ActivatedRoute,
+              private reportsService: ReportsService,
+              private sanitizer: DomSanitizer) {
     this.route.parent.data.subscribe((data: { loanDetailsData: any }) => {
       this.originalScheduleDetails = data.loanDetailsData.originalSchedule;
     });
+    this.loanId = this.route.parent.snapshot.paramMap.get('loanId');
   }
 
   ngOnInit() {
+  }
+
+  generate() {
+    const data = {
+      'output-type':	'PDF',
+      R_selectLoan:	this.loanId
+    };
+    this.reportsService.getPentahoRunReportData('Loan Repayment schedule', data, 'default', 'en', 'dd MMMM yyyy')
+      .subscribe( (res: any) => {
+        const contentType = res.headers.get('Content-Type');
+        const file = new Blob([res.body], {type: contentType});
+        const filecontent = URL.createObjectURL(file);
+        this.pentahoUrl = this.sanitizer.bypassSecurityTrustResourceUrl(filecontent);
+        this.showReport = true;
+      });
+  }
+
+  closeReport(){
+    this.showReport = false;
   }
 
 }
