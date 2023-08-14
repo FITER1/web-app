84543c79ad8ddfaf7f52883109d0447cb911389a
rjakech <rjakech@users.noreply.github.com>
2023-08-14 10:23:04 +0000

    Deploying to gh-pages from  @ ed47d769e38be828f632eaa80173371992013de6 🚀

--- assets/env.js
+++ assets/env.js
@@ -2,15 +2,21 @@
   window["env"] = window["env"] || {};
 
   // BackEnd Environment variables
-  window["env"]["fineractApiUrls"] = 'https://localhost:8443';
-  window["env"]["fineractApiUrl"]  = 'https://localhost:8443';
+  window["env"]["fineractApiUrls"] = '';
+  window["env"]["fineractApiUrl"]  = '';
 
   window["env"]["apiProvider"] = '/fineract-provider/api';
   window["env"]["apiVersion"]  = '/v1';
 
   window["env"]["fineractPlatformTenantId"]  = '';
+  window["env"]["fineractPlatformTenantIds"]  = '';
 
   // Language Environment variables
   window["env"]["defaultLanguage"] = '';
   window["env"]["supportedLanguages"] = '';
+
+  window['env']['preloadClients'] = '';
+
+  // Char delimiter to Export CSV options: ',' ';' '|' ' '
+  window['env']['defaultCharDelimiter'] = ',';
 })(this);
